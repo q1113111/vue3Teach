8ec2e69f898ecd024ef0fb00a91d19af8bd8fd3c
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-02-07 18:49:46 +0800

    toRaw makrRaw isReactive

--- src/views/BaseReactivity.vue
+++ src/views/BaseReactivity.vue
@@ -1,17 +1,36 @@
 <template>
-  456
+  {{ reactiveRaw }}
 </template>
 <script setup>
-import { readonly, reactive, ref } from 'vue'
+import { readonly, reactive, ref, isReactive, toRaw, markRaw } from 'vue'
 const original = reactive({ count: 0 })
 const copy = readonly(original)
 original.count++
 copy.count++ // 會報錯
 
+// 也可以複製ref
 const raw = {
   count: ref(123)
 }
 const copy2 = readonly(raw)
 console.log(raw.count.value) // 123
 console.log(copy2.count) // 123
+
+// isReactive
+const state = reactive({ name: 'Sam' })
+const plain = readonly({ name: 'mary' })
+console.log(isReactive(plain)) // -> false
+const stateCopy = readonly(state)
+console.log(isReactive(stateCopy)) // -> true
+
+// toRaw
+const toraw = { name: 'ss' }
+const reactiveRaw = reactive(toraw)
+console.log(toRaw(reactiveRaw) === toraw) // true
+
+// markRaw
+const mark = markRaw({})
+console.log(isReactive(reactive(mark))) // false
+const bar = reactive({ mark })
+console.log(isReactive(bar.mark))// false
 </script>
